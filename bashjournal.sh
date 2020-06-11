12b6b9b11d759368c7814ef0d4c329c1d8f9a283
kilomcorrido <53127617+kilomcorrido@users.noreply.github.com>
2020-06-11 14:25:42 -0600

    Added comments

--- bashjournal.sh
+++ bashjournal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Prepare terminal for text input
 echo -e "Type your entry : "
 read ENTRY
 
+#Echo input to file
 echo -e `date`"\n$ENTRY\n" >> ~/journal/`date "+%Y-%m-%d.txt"`
 echo "Entry saved to ~/journal/`date '+%Y-%m-%d.txt'`"
